3baa7a1f69b022131774dadc8b8d995b7f52de22
Azad <63344076+porfskylord@users.noreply.github.com>
2026-02-07 20:07:49 +0530

    created multiple pages using routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Routes, Route } from 'react-router';
 import { HomePage } from './pages/HomePage';
 import './App.css'
 
@@ -5,7 +6,10 @@ import './App.css'
 function App() {
   return(
     <>
-      <HomePage />
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path='checkout' element={<div>CheckOut</div>} />
+      </Routes>
     </>
   );
 }
