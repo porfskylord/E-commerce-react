3593cf89925b51414ef8fedf043fc7b7ae324dc2
Azad <63344076+porfskylord@users.noreply.github.com>
2026-02-11 20:59:01 +0530

    use data from backend api to genrate html

--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,15 +1,21 @@
 import axios from "axios";
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
-import { products } from "../../../starting-code/data/products";
 import CheckmarkIcon from "../../assets/images/icons/checkmark.png";
 import "./HomePage.css";
 
 export function HomePage() {
 
-  axios.get('http://localhost:3000/api/products')
+  const [products, setProducts] = useState([]);
+
+  useEffect(() =>{
+    axios.get('http://localhost:3000/api/products')
     .then((response) => {
-      console.log(response.data);
+      setProducts(response.data);
     });
+  },[]);
+
+  
   
 
   return (
