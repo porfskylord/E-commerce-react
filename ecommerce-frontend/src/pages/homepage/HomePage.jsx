031bd043fac0aabcb9f9881267032fb5984d4a51
Azad <63344076+porfskylord@users.noreply.github.com>
2026-02-13 00:57:28 +0530

    create unit test for format money

--- ecommerce-frontend/src/pages/homepage/HomePage.jsx
+++ ecommerce-frontend/src/pages/homepage/HomePage.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router";
 import { Header } from "../../components/Header";
 import { ProductsGrid } from "./ProductsGrid";
 import "./HomePage.css";
@@ -7,15 +8,18 @@ import "./HomePage.css";
 export function HomePage( {cart, loadCart} ) {
 
   const [products, setProducts] = useState([]);
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search')
 
   useEffect(() => {
     const fetchHomeData = async () => {
-      const response = await axios.get('/api/products')
+      const urlPath = search ? `/api/products?search=${search}` : '/api/products';
+      const response = await axios.get(urlPath);
       setProducts(response.data);
     };
 
     fetchHomeData();
-  },[]);
+  },[search]);
 
   return (
     <>
